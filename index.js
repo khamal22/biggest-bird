2701a913b3160b5dbbe14a502f3339c70a5ce060
khamal22 <yyeah3198@gmail.com>
2023-02-08 02:01:54 +0000

    add calculateDistance method

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 (function(window, _) {
     window.myLibraryName = window.myLibraryName || {
       numz: {},
-      phyz: {},
+      phyz: {
+
+       getDistance(pointA, pointB){
+          const
+          distanceX = pointB.x - pointA.x,
+          distanceY = pointB.y - pointA.y,
+          distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+          return distance;
+       }
+
+      },
     };
   }(window, window._));
